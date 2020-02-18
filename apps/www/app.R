99fe98784cdb6e4966f4d92675199ddfb7aea32b
Norris <niw2@cdc.gov>
2020-02-18 12:21:38 -0500

    First commit

---
+++ apps/www/app.R
@@ -0,0 +1,14 @@
+# Use ----------------------------------------
+
+library(shiny)
+
+
+addResourcePath('src','www')
+
+
+ui <- htmlTemplate ("index.html") #use this for shinyapps.io deployment
+
+
+server <- shinyServer(function(input, output) {})
+
+shinyApp(ui = ui, server = server)
